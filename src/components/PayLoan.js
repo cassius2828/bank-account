48ab73c671791d4525b8d6e1183055398d6ee8da
Cassius Reynolds <cassius2828@gmail.com>
2024-02-01 22:15:58 -0800

    removed suggestion errors

--- src/components/PayLoan.js
+++ src/components/PayLoan.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, {  useState } from "react";
 
 export const PayLoan = ({ dispatch, state }) => {
   // this local state allows me to clear the input field to give
--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,5 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faGithub,
+  //  faLinkedin
+   } from "@fortawesome/free-brands-svg-icons";
 
 export const Footer = () => {
   return (
